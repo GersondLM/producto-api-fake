9cec940f60df4eddd675e02b842e243ee2f0f28b
GersondLM <gdeleonm10@miumg.edu.gt>
2024-10-18 18:26:35 -0600

    Add files via upload

---
+++ proyecto-master/tienda/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiURL: 'https://fakestoreapi.com',
+  stripeAPIKey: 'pk_test_51QAI9UCx0lgZE9QQWYb6sbJ3nhm6zpWiUZmiNMFQML3F6tlCiazUFbeBDflPRn7E8aipct1HzvFlmPPTqS73eeae00Gyivse4e',
+  serverURL: 'http://localhost:4242',
+};
